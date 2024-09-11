84b687f4caca8ffad6c6cc8a4e2dc421ec9babbf
Yiling1201 <soireelaine@gmail.com>
2024-09-12 01:07:09 +0200

    created P5-Operators

---
+++ P5Operators/js/app.js
@@ -0,0 +1,33 @@
+let speedkmh = 72
+let speedms = speedkmh* 1000/3600;
+console.log(speedms);
+
+let minutes = 3;
+let seconds = minutes* 60;
+console.log(seconds);
+
+let divide= 11/4;
+console.log(divide);
+
+let modulo = 11 % 4;
+console.log(modulo);
+
+const radius= 2;
+const area= radius*radius*3.14;
+console.log(area)
+
+const number=11;
+const negation= -number;
+console.log(negation);
+
+const weight= 70;
+const height= 1.82;
+const BMI=weight/(height*height);
+console.log(BMI);
+
+const sideA=3;
+const sideB=4;
+const hypotenuse = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+console.log(hypotenuse);
+
+
