d6f6e6320cb9ed159cd4e3683c0e9880094788d8
Yiling1201 <soireelaine@gmail.com>
2024-09-17 07:22:14 +0200

    Refactor the code P4

---
+++ P6Types/js/app.js
@@ -0,0 +1,6 @@
+let numberOfBottles = 10;
+
+console.log(numberOfBottles + " green bottles hanging on the wall. " + numberOfBottles + " green bottles hanging on the wall.");
+
+console.log("And if one green bottle should accidentally fall, there will be " + (--numberOfBottles) + " green bottles hanging on the wall.");
+
