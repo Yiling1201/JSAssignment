845e57edbaba182419c0d45500a8b6534c83d2dd
Yiling1201 <soireelaine@gmail.com>
2024-09-17 11:34:16 +0200

    start p10

---
+++ P10OutputAndInput/js/app.js
@@ -0,0 +1,54 @@
+function multiplyByItself(number) {
+  return number * number;
+}
+console.log(multiplyByItself(4))
+
+
+const number1 = 2
+const number2 = 6
+function averageOfTwoNumbers(number1, number2) {
+  return (number1 + number2)/2;
+}
+console.log(averageOfTwoNumbers(number1, number2)); //
+
+
+
+const firstName = "Ben";
+const lastName = "Ting";
+function welcomeMessage(firstName, lastName) {
+  return "Welcome " + firstName + " " + lastName;
+}
+console.log(welcomeMessage(firstName, lastName));
+
+
+
+function welcome(firstName, lastName){
+return "Welcome" + firstName + " " +lastName;
+}
+console.log(welcome("Jane","Yo"))
+
+
+
+function getRandomInt(){
+  return Math.floor(Math.random()*10000);
+}
+console.log(getRandomInt());
+
+
+function convertNumber(number) {
+  if (number === 0) {
+    return "A";
+  }
+
+  if (number === 1) {
+    return "B";
+  }
+
+  if (number === 2) {
+    return "C";
+  }
+}
+
+console.log(convertNumber(0));
+console.log(convertNumber(1));
+console.log(convertNumber(2));
