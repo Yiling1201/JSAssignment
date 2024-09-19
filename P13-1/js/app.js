dca7a52729628a4f680f001e3c543fda28642c16
Yiling1201 <soireelaine@gmail.com>
2024-09-19 10:25:54 +0200

    Practice P13

--- P13-1/js/app.js
+++ P13-1/js/app.js
@@ -1,1 +1,16 @@
-const text = document.getElementById("text0");
+//route
+const textField0 = document.getElementById("text0");
+textField0.innerHTML = showStylizedScore(786);
+
+
+
+function showScore (inputNumber) {
+ return inputNumber * 100;//output is a  number
+}
+
+function showStylizedScore (scoreInput) {
+  return "Your Score is" + scoreInput * 100;
+
+}
+
+
